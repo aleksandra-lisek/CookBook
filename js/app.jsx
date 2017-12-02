2e6773826fc636dd19595eeef4efc2b8aa6fe236
aleksandra-lisek <olalisek.al@gmail.com>
2017-12-02 12:35:56 +0100

    files

---
+++ js/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    ReactDOM.render(
+       <App/>, document.getElementById('app'));
+
+
+
+
+});
